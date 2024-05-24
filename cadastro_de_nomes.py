e4c462a2097c323f3bc5e053d55194e742779232
Tiagolorenzo <tiago.lorenzo@hotmail.com>
2024-05-24 19:35:13 -0300

    first commit

---
+++ cadastro_de_nomes.py
@@ -0,0 +1,12 @@
+# lista de frutas
+frutas = ['maracujá', 'banana', 'maçã']
+
+# usuário informa o nome da nova fruta 
+nova_fruta = input('informe o nome da nova fruta: ')
+
+# nova fruta é inserida na lista
+frutas.append(nova_fruta)
+
+# exibe na telaanova lista
+for frutas in frutas
+print(frutas)
